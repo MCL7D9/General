af1400f9e9fbb51b4317937c26ef803d4f8028c0
MCL7D9 <101371148+MCL7D9@users.noreply.github.com>
2023-06-20 02:58:34 +0000

    a

---
+++ C/Test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void){
+    int a;
+    printf("What is your name?\n");
+    scanf(a);
+    printf("your name is %d",&a);
+    return 0;
+}
--- mcLib.h
+++
@@ -1,4 +0,0 @@
-#include M<iostream>
-using namespace std;
-int main(){
-}
