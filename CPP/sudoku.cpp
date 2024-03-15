e6c0ddd4e456d7104af02f07bb172da1e3e9e5ce
MCL7D9 <mcl7d9@outlook.com>
2024-03-15 20:43:57 +0800

    general

--- CPP/sudoku.cpp
+++ CPP/sudoku.cpp
@@ -1,6 +1,7 @@
 #include <cstdlib>
 #include <iostream>
 #include <ctime>
+#include <string>
 using namespace std;
 typedef short int si;
 
@@ -16,46 +17,126 @@ inline int rando(bool except[9],si upLim=9){
 }
 
 class sudoku{
+
       private:
-        si sudo[9][9];
+          si sudo[9][9], si record[81]={0},top=0;
       public:
       		
     	void printLs(){
-            cout<<"\t1\t2\t3\t4\t5\t6\t7\t8\t9";
-            for (int i=1;i<10;i++){
-             	cout<<endl<<i<<"\t";
+          cout<<"\t1\t2\t3\t4\t5\t6\t7\t8\t9";
+          for (int i=1;i<10;i++){
+               cout<<endl<<i<<"\t";
              	for (int j=0;j<9;j++){
              		cout<<sudo[i-1][j]<<"\t";
              	}
         	}
-		}
+	}
 		
-		void genPuzzle(int diff){
-			bool check[10]={1};
-			for (int i=0;i<9;i++){
-				for (int j=0;j<9;j++){
-					for (int l=0;l<9;l++){
-						check[l]=sudo[i][l];
-						check[l]=sudo[l][j];	
-					}
-					for (int x=1;x<4;x++){
-						for (int y=1;y<4;y++){
-							check[sudo[(((int)((x-1)/3))*3+x)][(((int)((y-1)/3))*3+y)]]=sudo[(((int)((x-1)/3))*3+x)][(((int)((y-1)/3))*3+y)];
-						}
-					}
-					sudo[i][j]=rando(check);
-				}
-			}
-			
-		}
+     void genPuzzle(){
+          bool check[10]={1};
+          for (int i=0;i<9;i++){
+               for (int j=0;j<9;j++){
+                    for (int l=0;l<9;l++){
+                         check[l]=sudo[i][l];
+                         check[l]=sudo[l][j];	
+                    }
+                    for (int x=1;x<4;x++){
+                         for (int y=1;y<4;y++){
+                              check[sudo[(((int)((x-1)/3))*3+x)][(((int)((y-1)/3))*3+y)]]=sudo[(((int)((x-1)/3))*3+x)][(((int)((y-1)/3))*3+y)];
+                         }
+                    }
+                    sudo[i][j]=rando(check);
+               }
+          }
+          for (int i=0;i<20;i++){
+               sudo[rando()][rando()]=0;
+          }
+          print("Puzzle Generated")
+     }
+
+     bool judge(){
+          for (int i=0;i<9;i++){
+               for (int j=0;j<9;j++){
+                    for (int x=0;x<9;x++){
+                         if ((sudo[i][j]==sudo[i][x])||(sudo[i][j]==sudo[x][j])){
+                              return 0;
+                         }
+                    }
+                    for (int x=1;x<4;x++){
+                         for (int y=1;y<4;y++){
+                              if (sudo[i][j]==sudo[((int)((i-1)/3))*3+y][((int)((j-1)/3))*3+x]){
+                                   print("Fail");
+                                   return 0;
+                              }
+                         }
+                    }
+               }
+          }
+          print("Success");
+          return 1;
+     }
+
+     bool input(bool a;si x,si y,si z){
+          char *x0,*y0;string temp;
+          x0=itoa(x);y0=itoa(y)
+          if (a==1&&sudo[x][y]==0){
+               sudo[x][y]=z;
+               record[top]=temp;
+               top++;
+               return 1;
+          }
+          if (a==1){
+               for (int i=0;i<top;i++){
+                    if record[i]==temp(
+                         sudo[x][y]=z;
+                         record[top]=temp;
+                         top++;
+                         return 1;
+                    )
+               }
+          }
+          if (a==0){
+               for (int i=0;i<top;i++){
+                    if record[i]==temp(
+                         sudo[x][y]=0;
+                         return 1;
+                    )
+               }
+          }
+          return 0;
+     }
 };
 
 int main(int argc, char *argv[]){
-    sudoku game;si diff;
-    cout<<"Welcome to sudoku!What is your name?";
-    cout<<"How do you want the game to be difficult?1-5";
-    cin>>diff;
-    game.genPuzzle(diff);
+    sudoku game;si diff;string inp;
+    cout<<"Welcome to sudoku!";
+    game.genPuzzle();
+    while ()
+    {
+          cin>>inp;bool ans;
+          if (inp[2]==c){
+               bool res;
+               res=game.judge();
+               if (res==1){
+                    print("You have won!");
+                    return 0;
+               }else{
+                    print("Wrong. Please Modify Your Answer");
+                    continue;
+               }
+
+          }
+          ans=input(inp[0],inp[1],inp[2]);
+          if (ans==1){
+               print("Number Modified Successfully");
+               game.printLs();
+          }else{
+               print("Number Update Failed");
+               game.printLs();
+          }
+
+    }
+    
     system("pause");
     return 0;
 }
